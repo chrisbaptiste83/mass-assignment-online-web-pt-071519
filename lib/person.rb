519ee28109330448cdfb1070b741f0ec21ab5e4f
Christopher Baptiste <chrisbaptiste83@gmail.com>
2019-08-23 20:15:16 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,5 +4,7 @@ class Person
 :wrist_size, :glove_size, :pant_length, :pant_width
   
   def initialize(attribute_hash) 
-    
+   attribute_hash.each {|key, value| self.send(("#{key}="), value)} 
+ end 
+  end 
 end
